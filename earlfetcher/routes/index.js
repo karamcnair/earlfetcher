26dc56c30e88c5823870972b35e7e048477f4760
Kara McNair <kara.mcnair@gmail.com>
2016-02-23 12:40:57 -0800

    Working retrieval of raw (ugly) HTML & display in UI

--- earlfetcher/routes/index.js
+++ earlfetcher/routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+var request = require('request');
+
+// var proxy = httpProxy.createProxyServer();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +12,23 @@ router.get('/', function(req, res, next) {
 	console.log("req.query : ",  req.query);
 	console.log('urlToFetch: ' + urlToFetch);
 
-  	res.render('index', { title: 'earlfetcher', theUrl: urlToFetch });
+	var retrievedHTML = "";
+
+	if (urlToFetch != undefined) {
+		request(urlToFetch, function (error, response, body) {
+	 		if (!error && response.statusCode == 200) {
+	   		 	console.log(body); // Show the HTML for the Google homepage.	
+   		 	  	res.render('index', { title: 'earlfetcher', theUrl: urlToFetch, retrievedHTML: body });
+	  		} else if (!error) {
+	  			res.render('index', { title: 'earlfetcher', theUrl: urlToFetch, retrievedHTML: "Could not fetch HTML: " + response.statusCode + " " + error + "." } );
+	  		} else {
+	  			console.log(error);
+	  			res.render('index', { title: 'earlfetcher', theUrl: urlToFetch, retrievedHTML: error} );
+	  		}
+		});	
+	} else {
+  		res.render('index', { title: 'earlfetcher', theUrl: urlToFetch, retrievedHTML: "" });
+  	};
 });
 
 // router.get('/getHTML', function(req, res, next) {
@@ -18,5 +38,24 @@ router.get('/', function(req, res, next) {
 
 // 	res.render('html', { title: 'no thanks, I already got one!', theUrl: urlToFetch});
 // });
+
+function getHTML(urlToFetch) {
+
+	// returns array of [host, port, path]. Port value is not required.
+	// I'm going to only support HTTP URLs at the moment.
+
+	// var parsedUrl = parseUrl(urlToFetch);
+	
+	// if (parsdeUrl.length != 3) {
+	// 	return "Sorry, we were unable to parse: " + urlToFetch;
+	// }
+	// else
+	request('http://www.google.com', function (error, response, body) {
+ 		if (!error && response.statusCode == 200) {
+   		 	console.log(body); // Show the HTML for the Google homepage.
+   		 	return body;
+  		};
+	});
+};
 
 module.exports = router;
